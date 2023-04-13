76d46ada49cd4e54a0ef9a281f4608415a148f1f
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-04-13 14:34:46 +0300

    create User, Post and Comment models

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: {in: 4..12}
+    validates :password, presence: true, length: {in: 4..16}
+
+    has_many :posts
+end
